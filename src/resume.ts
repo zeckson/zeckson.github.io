219337aa740cb8fe0aedee6e827f12a284190cf4
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2021-05-04 22:33:56 +0300

    simplify

--- src/resume.ts
+++ src/resume.ts
@@ -1,17 +1,6 @@
 import {pug, yaml} from "../deps.ts";
 
-const parseFile = async (filePath: string) => {
-  /** Loading the file. */
 // @ts-ignore
-  const yamlFile = await Deno.readFile(filePath);
-
-  /** Decoding the file text. */
-  const yamlText = (new TextDecoder("utf-8")).decode(yamlFile);
-
-  /** Returning parsed yaml as an object. */
-  return await yaml.parse(yamlText);
-}
-
-console.dir(await parseFile(`./resume.yaml`));
+console.dir(await yaml.parse(await Deno.readTextFile(`./resume.yaml`)));
 console.dir(pug.name);
 console.dir(pug.compileFile);
